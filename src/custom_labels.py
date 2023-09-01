6b742f293d25eb5092a31dd2204a3f6112e03aa2
Mandadapu <lmandadapu@netquest.com>
2023-09-01 19:25:47 +0200

    [REF] refactor custom labels creation

---
+++ src/custom_labels.py
@@ -0,0 +1,26 @@
+import torch
+
+label_mapping = {
+    0: "upper_part",
+    1: "bottom_part",
+    2: "onepiece",
+    3: "footwear",
+    4: "bags"
+}
+
+# Applying custom labelling as per the project requirements
+def apply_custom_labels(labels):
+    custom_labels = []
+    for label in labels:
+        if label in [0, 2, 4, 6]:
+            custom_labels.append(0)
+        elif label == 1:
+            custom_labels.append(1)
+        elif label == 3:
+            custom_labels.append(2)
+        elif label in [5, 7, 9]:
+            custom_labels.append(3)
+        elif label == 8:
+            custom_labels.append(4)
+    return torch.tensor(custom_labels)
+
